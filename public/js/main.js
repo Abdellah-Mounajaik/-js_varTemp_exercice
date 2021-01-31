34dff9f8b77403b596c8cf17a5b22342abbccb3b
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 01:11:03 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,70 @@
+// let a = 3;
+// let b = 1;
+// let c = 2;
+// let temp;
+// temp = a;
+// a = b
+// b = c 
+// c = temp;
+// console.log(a,b,c);
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4; 
+// let temp;
+
+// temp = d;
+// d = a;
+// a = temp;
+// temp = c;
+// c = b;
+// b = temp;
+// console.log(a, b, c, d);
+
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+// let temp;
+// temp = a
+// a = b
+// b = c
+// c = d
+// d = temp
+// console.log(a, b, c, d);
+
+// let tableau = [1, 2, 3, 4];
+
+// let temp;
+// temp = tableau[0];
+
+// tableau[0] = tableau[3];
+// tableau[3] = temp;
+// temp = tableau[1];
+// tableau[1] = tableau[2];
+// tableau[2] = temp;
+
+// console.log(tableau);
+
+// let a = 1
+// let b = 2
+// let c = 3
+// let d = 4
+// let e = 5
+// let f = 6
+// let temp;
+// let temp1;
+// let temp2;
+
+// temp = a;
+// temp1 = b;
+// temp2 = c
+// a = d
+// b = e
+// c = temp
+// d = temp1
+// e = f
+// f = temp2
+// console.log(a,b,c,d,e,f);
